d1e99dba41aff9b148bef1b51a4db7485c5364bf
asankasampath83 <asanka.maha@gmail.com>
2025-09-23 11:45:45 +0300

    I have finished above 4 task as a lecture 3 asignments

--- lecture3/task01.py
+++ lecture3/task01.py
@@ -1,1 +1,14 @@
-print(3)
+Print("add to cart")
+
+if not available:
+    print("remove from the cart.")
+while True:
+    item = input("add item to the cart" :)
+if item = = "":
+
+
+Remove from the cart
+
+Check the cart
+
+stop
